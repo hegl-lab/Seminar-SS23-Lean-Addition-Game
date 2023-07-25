03583fb0b258678c27ef0b7c8f5299dc80bd6263
add-iv <frederick.2000@gmx.de>
2023-07-25 13:37:35 +0200

    import new proofs

--- Game/Levels/Tutorial.lean
+++ Game/Levels/Tutorial.lean
@@ -1,3 +1,11 @@
+import Game.Levels.Tutorial.Level_14
+import Game.Levels.Tutorial.Level_13
+import Game.Levels.Tutorial.Level_12
+import Game.Levels.Tutorial.Level_11
+import Game.Levels.Tutorial.Level_10
+import Game.Levels.Tutorial.Level_9
+import Game.Levels.Tutorial.Level_8
+import Game.Levels.Tutorial.Level_7
 import Game.Levels.Tutorial.Level_6
 import Game.Levels.Tutorial.Level_5
 import Game.Levels.Tutorial.Level_4
