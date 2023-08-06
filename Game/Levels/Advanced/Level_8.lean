f9bc9e62a8cd7c5280f13c021895cd28dbafcb0d
add-iv <frederick.2000@gmx.de>
2023-08-06 23:39:07 +0200

    add last boss

--- Game/Levels/Advanced/Level_8.lean
+++ Game/Levels/Advanced/Level_8.lean
@@ -5,17 +5,47 @@ Level 8
 Title "Bringing it all together"
 
 Introduction "
+Now that we have learned the definitions, lets look at how Or and And are defined.
 
+    inductive And (a b : Prop) : Prop where
+    | intro : a → b → And a b
+
+    inductive Or (a b : Prop) : Prop where
+    | inl : a → Or a b
+    | inr : b → Or a b
+
+The inductive type is a type that can have multiple constructors.
+Intuitively And a b is true if both a and b are true, which can be written as a → b → And a b.
+This is exactly what the intro constructor does, it takes a proof of a and a proof of b and returns a proof of And a b.
+Or a b is true if either a or b is true, so we have two constructors, one for each case.
+
+Now lets proove de Morgan's law.
+Use Iff.intro to split the goal into two implications.
 "
 
 Statement
-"p ∨ q → q ∨ p"
-    (p q: Prop) : p ∨ q → q ∨ p:= by
+"¬(p ∨ q) ↔ (¬p ∧ ¬q)"
+    (p q: Prop) : ¬(p ∨ q) ↔ (¬p ∧ ¬q):= by
+    Hint "apply Iff.intro"
+    apply Iff.intro
     intro h1
-    apply h1.elim
+    apply And.intro
     intro h2
-    exact Or.intro_right q h2
+    Hint "use apply h1 since h1 is (p ∨ q) → False"
+    apply h1
+    apply Or.inl
+    trivial
     intro h2
-    exact Or.intro_left p h2
+    apply h1
+    apply Or.inr
+    trivial
+    intro h1
+    intro h2
+    Hint "apply Or.elim"
+    apply Or.elim h2
+    exact h1.left
+    exact h1.right
+
+NewLemma Iff.intro
 
 NewDefinition And Or Not
