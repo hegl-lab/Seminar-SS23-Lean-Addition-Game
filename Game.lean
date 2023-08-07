2555381c5901b28fb7606606b03b437561197f43
add-iv <frederick.2000@gmx.de>
2023-08-07 14:50:17 +0200

    add description

--- Game.lean
+++ Game.lean
@@ -3,11 +3,11 @@ import GameServer.Commands
 import Game.Levels.Tutorial
 import Game.Levels.Advanced
 
-Title "Natural Number Game"
+Title "Addgame"
 Introduction
 "
-
-
+This short game will show you the basics of propositional logic in Lean.
+click on Tutorial World to start.
 "
 
 Dependency Tutorial → Advanced
